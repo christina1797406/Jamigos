ffb3ffa0aa94ac13997b9e2260d37790d615cf6d
Samira <a1852922@adelaide.edu.au>
2025-05-15 07:35:59 +0000

    updated style

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -1,22 +1,3 @@
-const playlists = [
-  { name: "Playlist 1", songs: "10 Songs", image: "assets/playlist1.jpg" },
-  { name: "Playlist 2", songs: "20 Songs", image: "assets/playlist2.jpg" },
-  { name: "Playlist 3", songs: "5 Songs", image: "assets/playlist3.jpg" },
-  { name: "Playlist 4", songs: "1 Song", image: "assets/playlist4.jpg" },
-];
-
-const container = document.querySelector(".playlists");
-playlists.forEach(pl => {
-  const card = document.createElement("div");
-  card.className = "playlist-card";
-  card.innerHTML = `
-      <img src="${pl.image}" alt="${pl.name}">
-      <p>${pl.name}</p>
-      <small>${pl.songs}</small>
-    `;
-  container.appendChild(card);
-});
-
 document.addEventListener("DOMContentLoaded", () => {
   const avatar = document.getElementById("avatar");
   const fileInput = document.getElementById("avatarInput");
