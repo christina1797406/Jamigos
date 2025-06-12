d684a8c29c5e35f2787a7ea269b8df809eede316
christina1797406 <a1797406@adelaide.edu.au>
2025-06-12 02:41:27 +0000

    copied files from feature/account-setup for testing

---
+++ accSetup.js
@@ -0,0 +1,68 @@
+const emailInput = document.querySelector('.email-input');
+const usernameInput = document.querySelector('.username-input');
+const passwordInput = document.querySelector('.password-input');
+const messageEl = document.getElementById('signup-message');
+const signUpBtn = document.querySelector('.signup-button');
+
+// User first-time sign in
+signUpBtn.addEventListener('click', () => {
+  const email = emailInput.value.trim();
+  const username = usernameInput.value.trim();
+  const password = passwordInput.value;
+
+  if (!email || !username || !password) {
+    messageEl.textContent = 'Please fill out all fields.';
+    messageEl.style.color = 'red';
+    return;
+  }
+
+  fetch('http://localhost:3000/addUser', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ email, username, password })
+  })
+    .then(res => res.json())
+    .then(data => {
+      if (data.error) {
+        messageEl.textContent = 'Email/username already in use';
+        messageEl.style.color = 'red';
+      } else {
+        messageEl.textContent = 'Signup successful!';
+        messageEl.style.color = 'green';
+        emailInput.value = '';
+        usernameInput.value = '';
+        passwordInput.value = '';
+
+        // Redirect to dashboard
+        // only works if the files are in the same branch so after merging
+        setTimeout(() => {
+          window.location.href = '/index.html';
+        }, 2500); // after 2.5 seconds
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      messageEl.textContent = 'Server error. Try again later.';
+      messageEl.style.color = 'red';
+    });
+});
+
+// Google sign-in setup
+function handleGoogleResponse(res) {
+  const credential = res.credential;
+
+  // Send user data to server
+  fetch('http://localhost:3000/auth/google', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ credential })
+  })
+    .then(res => res.json())
+    .then(data => {
+      console.log('Google login response:', data);
+    })
+    .catch(err => {
+      console.error('Google login failed:', err);
+    });
+}
+window.handleGoogleResponse = handleGoogleResponse;
---
+++ databases.sql
@@ -0,0 +1,52 @@
+-- account_manager
+CREATE DATABASE IF NOT EXISTS account_manager;
+USE account_manager;
+CREATE TABLE IF NOT EXISTS Users(
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    user_password VARCHAR(255) NOT NULL,
+    user_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- user profile
+CREATE TABLE IF NOT EXISTS User_Profile(
+    profile_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    user_bio TEXT,
+    display_name VARCHAR(100),
+    profile_pic VARCHAR(255) DEFAULT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+
+);
+
+-- oauth
+CREATE DATABASE IF NOT EXISTS Oauth;
+USE Oauth;
+CREATE TABLE IF NOT EXISTS Oauth_token(
+    oauth_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    oauth_provider VARCHAR(50) NOT NULL,
+    access_token TEXT NOT NULL,
+    refresh_token TEXT DEFAULT NULL,
+    token_expiration DATETIME,
+    token_creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    token_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES account_manager.Users(user_id) ON DELETE CASCADE
+);
+
+-- spotify
+CREATE DATABASE IF NOT EXISTS spotify;
+USE spotify;
+CREATE TABLE IF NOT EXISTS spotify_info(
+    spotify_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    spotify_account_id VARCHAR(255) NOT NULL,
+    spotify_username VARCHAR(255) NOT NULL,
+    spotify_access_token TEXT NOT NULL,
+    spotify_refresh_token TEXT DEFAULT NULL,
+    spotify_plan VARCHAR(50) DEFAULT NULL,
+    access_token_creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    access_token_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES account_manager.Users(user_id) ON DELETE CASCADE
+);
