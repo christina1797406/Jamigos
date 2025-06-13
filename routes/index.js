391061a868b7bda2c207e4d9037091bbea51ebdd
oveena-uoa <a1882491@adelaide.edu.au>
2025-06-13 01:40:04 +0000

    updated login path

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('Login', { title: 'Login' });
 });
 
 const SpotifyWebApi = require('spotify-web-api-node');
